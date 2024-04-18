af12d4565ffef3b96beeb92f72581ebfd0718c58
RohanKumarMainali <rohanmainali39@gmail.com>
2024-04-18 12:10:57 +0545

    feat(): add copilot and java setup

--- init.lua
+++ init.lua
@@ -2,3 +2,10 @@
 require("config.lazy")
 vim.api.nvim_command("set spelllang=en_us")
 vim.api.nvim_command("set spell")
+vim.keymap.set("i", "<C-r>", 'copilot#Accept("<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+
+vim.g.copilot_no_tab_map = true
+vim.b.copilot_enabled = false
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,13 +1,3 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  opts = {
-    suggestion = { enabled = true },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
+  "github/copilot.vim",
 }
